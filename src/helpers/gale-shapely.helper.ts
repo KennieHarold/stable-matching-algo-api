f57cf2976b02cea1099f7e3aaf897c7ad4e6a203
Kennie Harold <kennie.harold@gmail.com>
2022-05-03 20:24:31 +0800

    Added helpers with types

---
+++ src/helpers/gale-shapely.helper.ts
@@ -0,0 +1,117 @@
+// https://github.com/alextanhongpin/stable-marriage-problem/blob/master/javascript/stable-matching.js
+
+type PreferenceType = string[];
+type TuplePreferenceType = [string, PreferenceType];
+type GaleParamType = TuplePreferenceType[];
+
+const toObject = (arr: GaleParamType) => {
+  return arr.reduce((obj, [head, tail]) => {
+    obj[head] = tail;
+    return obj;
+  }, {});
+};
+
+const takeFirst = (arr: GaleParamType) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  return arr.map(([head, _]) => head);
+};
+
+const createHashTable = (arr: GaleParamType) => {
+  return arr.reduce((obj, [head, tail]) => {
+    if (!obj[head]) {
+      obj[head] = {};
+    }
+
+    return tail.reduce((_obj, item, score) => {
+      _obj[head][item] = score;
+      return _obj;
+    }, obj);
+  }, {});
+};
+
+class GaleShapely {
+  maleNames: PreferenceType;
+  maleChoices: TuplePreferenceType | Record<string, PreferenceType>;
+  scores: Record<string, Record<string, number>>;
+
+  constructor(males: GaleParamType, females: GaleParamType) {
+    this.maleNames = takeFirst(males);
+    this.maleChoices = toObject(males);
+    this.scores = createHashTable(females);
+  }
+
+  compareScore(male: string, female: string) {
+    return (this.scores[male] && this.scores[male][female]) || Infinity;
+  }
+
+  engage(state: Record<string, string>, male: string, female: string) {
+    state[male] = female;
+    state[female] = male;
+  }
+
+  breakup(state: Record<string, string>, male: string) {
+    state[male] = '';
+  }
+
+  currentPartner(state: Record<string, string>, person: string) {
+    return state[person];
+  }
+
+  isSingle(state: Record<string, string>, partner: string) {
+    return state[partner] === undefined;
+  }
+
+  getPreference(male: string) {
+    return (this.maleChoices[male] && this.maleChoices[male][0]) || null;
+  }
+
+  updatePreferences(male: string) {
+    const preferences = [...this.maleChoices[male]];
+    this.maleChoices[male] = preferences.slice(1, preferences.length);
+  }
+
+  terminationCondition(state) {
+    const currentMatches = this.maleNames.filter((name) => {
+      return state[name];
+    }).length;
+    return currentMatches === this.maleNames.length;
+  }
+
+  match(initialState = {}) {
+    const names = this.maleNames;
+    const loop = (initialState = {}) => {
+      return names.reduce((state, male) => {
+        if (!this.isSingle(state, male)) {
+          return state;
+        }
+        const female = this.getPreference(male);
+        if (this.isSingle(state, female)) {
+          this.engage(state, male, female);
+        } else {
+          const currMale = this.currentPartner(state, female);
+          const score1 = this.compareScore(male, female);
+          const score2 = this.compareScore(currMale, female);
+
+          if (score1 < score2) {
+            this.breakup(state, currMale);
+            this.engage(state, male, female);
+          }
+        }
+        this.updatePreferences(male);
+        return state;
+      }, initialState);
+    };
+
+    const state = loop(initialState);
+
+    if (!this.terminationCondition(state)) {
+      return {...state, ...this.match(state)};
+    }
+
+    return state;
+  }
+}
+
+module.exports = (male: GaleParamType, females: GaleParamType) => {
+  return new GaleShapely(male, females);
+};
---
+++ src/helpers/irvings.helper.ts
@@ -0,0 +1,187 @@
+// https://github.com/gfornari/stable-roommates-problem/blob/master/stable-roommates-algorithm.js
+
+type SN = string | number;
+type IrvingsParamType = string[][];
+
+class Irvings {
+  computeMatches = (preferences: IrvingsParamType) => {
+    const {prefs, accepts} = this.firstPhase(preferences);
+    const _prefs = this.secondPhase(prefs, accepts);
+
+    return this.thirdPhase(_prefs);
+  };
+
+  rejectSymmetrically = (
+    i: SN,
+    j: SN,
+    prefs: IrvingsParamType | Record<string, string>,
+  ) => {
+    const indexJ = prefs[i].indexOf(j);
+    if (indexJ > -1) prefs[i].splice(indexJ, 1);
+    const indexI = prefs[j].indexOf(i);
+    if (indexI > -1) prefs[j].splice(indexI, 1);
+  };
+
+  acceptProposal = (
+    i: SN,
+    preferred: SN,
+    proposals: IrvingsParamType | Record<string, string>,
+    accepts: IrvingsParamType | Record<string, string>,
+  ) => {
+    proposals[i] = preferred;
+    accepts[preferred] = i;
+  };
+
+  wouldBreakup = (
+    preferred: SN,
+    i: SN,
+    accepts: IrvingsParamType | Record<string, string>,
+    prefs: IrvingsParamType,
+  ) => {
+    const indexCurrentProposal = prefs[preferred].indexOf(accepts[preferred]);
+    const indexNewProposal = prefs[preferred].indexOf(i);
+    return indexNewProposal < indexCurrentProposal;
+  };
+
+  breakupFor = (
+    preferred: SN,
+    i: SN,
+    proposals: IrvingsParamType | Record<string, string>,
+    accepts: IrvingsParamType | Record<string, string>,
+    prefs: IrvingsParamType | Record<string, string>,
+  ) => {
+    proposals[i] = preferred;
+    const oldAccept = accepts[preferred];
+    accepts[preferred] = i;
+    this.rejectSymmetrically(preferred, oldAccept, prefs);
+    return oldAccept;
+  };
+
+  firstPhase = (prefs: IrvingsParamType) => {
+    const unmatched = [...Array(prefs.length).keys()];
+
+    const proposals = {};
+    const accepts = {};
+
+    while (unmatched.length > 0) {
+      const i = unmatched[0];
+      const preferred = prefs[i][0];
+
+      if (!accepts[preferred]) {
+        // the preferred is free
+        this.acceptProposal(i, preferred, proposals, accepts);
+        unmatched.shift();
+      } else if (this.wouldBreakup(preferred, i, accepts, prefs)) {
+        // preferred is willing to breakup
+        const preferredOldAccept = this.breakupFor(
+          preferred,
+          i,
+          proposals,
+          accepts,
+          prefs,
+        );
+        unmatched[0] = preferredOldAccept;
+      } else {
+        this.rejectSymmetrically(i, preferred, prefs);
+      }
+    }
+
+    return {prefs, accepts};
+  };
+
+  secondPhase = (
+    prefs: IrvingsParamType,
+    accepts: IrvingsParamType | Record<string, string>,
+  ) => {
+    for (let i = 0; i < prefs.length; i++) {
+      const indexAccept = prefs[i].indexOf(accepts[i]);
+      if (indexAccept > -1) {
+        // deep copy, just to be sure
+        const toRemove = JSON.parse(
+          JSON.stringify(prefs[i].slice(indexAccept + 1)),
+        );
+        toRemove.forEach((r) => this.rejectSymmetrically(i, r, prefs));
+      }
+    }
+
+    return prefs;
+  };
+
+  getRotation = (
+    i: SN,
+    prefs: IrvingsParamType,
+    secondChoices: string[],
+    lastChoices: SN[] = [],
+  ) => {
+    if (
+      lastChoices.slice(0, -1).indexOf(lastChoices[lastChoices.length - 1]) !==
+      -1
+    ) {
+      // preference cycle
+      return {secondChoices, lastChoices};
+    }
+
+    const second = prefs[i][1];
+
+    // at a given point in the recursion, we find that the current i has no second
+    // preference, hence there are no other rotations to remove, thus no stable matching exists.
+    // NOTE: this is just an intuition without actual proof
+    if (second === undefined) {
+      return {secondChoices: null, lastChoices: null};
+    }
+
+    secondChoices.push(second);
+    const secondLastIndex = prefs[second].length - 1;
+    const secondLast = prefs[second][secondLastIndex];
+    lastChoices.push(secondLast);
+
+    return this.getRotation(secondLast, prefs, secondChoices, lastChoices);
+  };
+
+  removeRotation = (
+    secondChoices: string[],
+    lastChoices: string[],
+    prefs: IrvingsParamType,
+  ) => {
+    let i = lastChoices.indexOf(lastChoices[lastChoices.length - 1]);
+
+    for (i++; i < lastChoices.length; i++) {
+      this.rejectSymmetrically(secondChoices[i - 1], lastChoices[i], prefs);
+    }
+  };
+
+  thirdPhase = (prefs: IrvingsParamType) => {
+    let i = 0;
+
+    while (i < prefs.length) {
+      if (prefs[i].length === 1) {
+        i++;
+      } else {
+        const {secondChoices, lastChoices} = this.getRotation(
+          i,
+          prefs,
+          [],
+          [i],
+        );
+
+        // no stable matching exists
+        if (secondChoices === null || lastChoices === null) {
+          return null;
+        }
+
+        this.removeRotation(secondChoices, lastChoices, prefs);
+
+        // if any list becomes empty, return null (no stable matching exists)
+        if (prefs.some((p) => p.length === 0)) {
+          return null;
+        }
+
+        i = 0;
+      }
+    }
+
+    return prefs;
+  };
+}
+
+module.exports = () => new Irvings();
